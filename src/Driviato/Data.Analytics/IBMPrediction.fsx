4434128d9849fa4e428ab953ebe40980f589532d
jamie_dixon <jamie@tenfingersfree.com>
2015-04-19 11:33:13 -0400

    Added IBM Watson Translation Service

---
+++ src/Driviato/Data.Analytics/IBMPrediction.fsx
@@ -0,0 +1,25 @@
+﻿#r @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.Net.Http.dll"
+#r @"..\packages\Microsoft.AspNet.WebApi.Client.5.2.2\lib\net45\System.Net.Http.Formatting.dll"
+ 
+open System
+open System.Net.Http
+open System.Net.Http.Headers
+open System.Net.Http.Formatting
+open System.Collections.Generic
+ 
+let serviceName = "machine_translation"
+let baseUrl = "http://wex-mt.mybluemix.net/resources/translate"
+let userName = "youNameHere@aol.com"
+let password = "yourPasswordHere"
+let authKey = userName + ":" + password
+ 
+let client = new HttpClient()
+client.DefaultRequestHeaders.Authorization <- new AuthenticationHeaderValue("Basic",authKey)
+ 
+let input = new Dictionary<string,string>()
+input.Add("text","This is a test")
+input.Add("sid","mt-enus-eses")
+let content = new FormUrlEncodedContent(input)
+ 
+let result = client.PostAsync(baseUrl,content).Result
+let resultContent = result.Content.ReadAsStringAsync().Result
---
+++ src/Driviato/Data.Analytics/IBMWatsonTranslationService.fs
@@ -0,0 +1,30 @@
+﻿namespace Data.Analytics
+
+open System
+open System.Net.Http
+open System.Net.Http.Headers
+open System.Net.Http.Formatting
+open System.Collections.Generic
+
+type IBMWatsonTranslationService() = 
+    member this.InvokeService (phrase, language) = async {
+        let serviceName = "machine_translation"
+        let baseUrl = "http://wex-mt.mybluemix.net/resources/translate"
+        let userName = "youNameHere@aol.com"
+        let password = "yourPasswordHere"
+        let authKey = userName + ":" + password
+ 
+        let client = new HttpClient()
+        client.DefaultRequestHeaders.Authorization <- new AuthenticationHeaderValue("Basic",authKey)
+ 
+        let input = new Dictionary<string,string>()
+        input.Add("text",phrase)
+        input.Add("sid",language)
+        let content = new FormUrlEncodedContent(input)
+ 
+        let result = client.PostAsync(baseUrl,content).Result
+        let resultContent = result.Content.ReadAsStringAsync().Result
+        return resultContent 
+    }
+
+
