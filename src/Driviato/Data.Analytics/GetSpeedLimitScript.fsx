1a452bb720244e87a0946dc417e6d63526b52c73
David Green <davidgreen24@gmail.com>
2015-04-19 09:29:35 -0400

    updating broken f#. data references
    updating broken f#. data references

--- src/Driviato/Data.Analytics/GetSpeedLimitScript.fsx
+++ src/Driviato/Data.Analytics/GetSpeedLimitScript.fsx
@@ -1,9 +1,9 @@
 ﻿
-#r "C:/Users/jamie/Desktop/4Gone.DriveSafe.Analytics/packages/FSharp.Data.2.2.0/lib/net40/FSharp.Data.dll"
+#r "C:/Github/ForgoneConclusion/src/Driviato/packages/FSharp.Data.2.2.0/lib/net40/FSharp.Data.dll"
 
 open FSharp.Data
 
-type SpeedLimit = JsonProvider<"C:\Users\jamie\Desktop\4Gone.DriveSafe.Analytics\Data\GoogleSpeedLimit.json">
-let speedLimits = SpeedLimit.Load("C:\Users\jamie\Desktop\4Gone.DriveSafe.Analytics\Data\GoogleSpeedLimit.json");
+type SpeedLimit = JsonProvider<"../Data/GoogleSpeedLimit.json">
+let speedLimits = SpeedLimit.Load("../Data/GoogleSpeedLimit.json");
 let lastSpeedLimit = speedLimits.SpeedLimits |> Seq.head
 lastSpeedLimit.SpeedLimit
