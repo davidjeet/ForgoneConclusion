a68f804e12cdbf3824355828c4ef28dd1616ee78
jamie_dixon <jamie@tenfingersfree.com>
2015-04-18 22:50:35 -0400

    Added BrainTree Debit Service

---
+++ src/Driviato/Data.Analytics/BrainTreeDebitService.fs
@@ -0,0 +1,21 @@
+﻿namespace Data.Analytics
+
+open System
+open Braintree
+open Data.Model
+
+type BrainTreeDebitService() = 
+    interface IDebitService with 
+        member this.DebitAccount(customerId, token, amount) = 
+            let gateway = new BraintreeGateway()
+            gateway.Environment <- Environment.SANDBOX
+            gateway.MerchantId <- "rfv2xwcbcwrpdnqk"
+            gateway.PublicKey <- "qgyf3xzzv3y9rczd"
+            gateway.PrivateKey <- "765479fccc7bae123b912ca23206fe09"
+
+            let transaction = new TransactionRequest()
+            transaction.Amount <- amount
+            transaction.CustomerId <- customerId
+            transaction.PaymentMethodToken <- token
+            gateway.Transaction.Sale(transaction) |> ignore
+
